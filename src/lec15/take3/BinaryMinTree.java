8710d6d522657c1883e9be8ac5f199ece2c8a5cd
Ketan Mayer-Patel <kmp@cs.unc.edu>
2020-03-05 13:57:42 -0500

    Update

--- src/lec15/take3/BinaryMinTree.java
+++ src/lec15/take3/BinaryMinTree.java
@@ -7,6 +7,7 @@ public class BinaryMinTree<V, P extends Comparable<P>> implements PriorityQueue<
 	private BinaryMinTree<V,P> _right;
 	private boolean _is_empty;
 	private int _height;
+	private int _size;
 	
 	public BinaryMinTree() {
 		_is_empty = true;
@@ -14,6 +15,7 @@ public class BinaryMinTree<V, P extends Comparable<P>> implements PriorityQueue<
 		_left = null;
 		_right = null;
 		_height = -1;
+		_size = 0;
 	}
 	
 	public BinaryMinTree(Prioritized<V,P> value) {
@@ -22,6 +24,7 @@ public class BinaryMinTree<V, P extends Comparable<P>> implements PriorityQueue<
 		_right = new BinaryMinTree<V,P>();
 		_is_empty = false;
 		_height = 0;
+		_size = 1;
 	}
 	
 	@Override
@@ -32,22 +35,25 @@ public class BinaryMinTree<V, P extends Comparable<P>> implements PriorityQueue<
 	
 	@Override
 	public V dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		Prioritized<V,P> pv = dequeuePrioritized();
+		return pv.getValue();
 	}
 
 	@Override
 	public V getMin() {
-		// TODO Auto-generated method stub
-		return null;
+		if (_is_empty) {
+			throw new UnsupportedOperationException();
+		}
+		
+		Prioritized<V,P> pv = getPrioritizedMin();
+		return pv.getValue();
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _size;
 	}
-
+	
 	private void enqueuePrioritized(Prioritized<V,P> value) {
 		if (_is_empty) {
 			_root_value = value;
@@ -70,9 +76,75 @@ public class BinaryMinTree<V, P extends Comparable<P>> implements PriorityQueue<
 			
 			_height = Math.max(_left.getHeight(), _right.getHeight()) + 1;
 		}
+		_size++;
 	}
 
+	private Prioritized<V,P> dequeuePrioritized() {
+	
+		if (_is_empty) {
+			throw new UnsupportedOperationException();
+		}
+		
+		Prioritized<V,P> result = _root_value;
+		
+		if (size() == 1) {
+			// Case 1. Become empty.
+			_is_empty = true;
+			_root_value = null;
+			_left = null;
+			_right = null;
+			_height = -1;
+			return result;
+			
+		} else if (_left.isEmpty()) {
+			// Case 2, left empty. Take from right.
+			_root_value = _right.dequeuePrioritized();
+			
+		} else if (_right.isEmpty()) {
+			// Case 2, right empty. Take from left.
+			_root_value = _left.dequeuePrioritized();
+			
+		} else {
+			// Case 3, both children non-empty
+			Prioritized<V,P> left_min = _left.getPrioritizedMin();
+			Prioritized<V,P> right_min = _right.getPrioritizedMin();
+			
+			if (left_min.getPriority().compareTo(right_min.getPriority()) < 0) {
+				_root_value = _left.dequeuePrioritized();
+			} else {
+				_root_value = _right.dequeuePrioritized();
+			}
+			
+			if (_left.getHeight() != _right.getHeight()) {
+				BinaryMinTree<V,P> taller = 
+						(_left.getHeight() > _right.getHeight()) ? _left : _right;
+				BinaryMinTree<V,P> shorter = 
+						(_left.getHeight() < _right.getHeight()) ? _left : _right;
+				
+				while (taller.getHeight() - shorter.getHeight() > 1) {
+					shorter.enqueuePrioritized(taller.dequeuePrioritized());
+				}
+			}								
+		}  
+		
+		_height = Math.max(_left.getHeight(), _right.getHeight())+1;
+		_size--;
+		return result;
+	}
+
+	private Prioritized<V,P> getPrioritizedMin() {		
+		if (_is_empty) {
+			throw new UnsupportedOperationException();
+		}
+
+		return _root_value;
+	}
+	
 	private int getHeight() {
 		return _height;
+	}
+	
+	private boolean isEmpty() {
+		return _is_empty;
 	}
 }
